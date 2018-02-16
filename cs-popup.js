acace12b8348826f81c7b0b2932bf193dcd7bf17
Jason Purdy <jason@journalistic.com>
2018-02-16 14:38:57 -0500

    work in progress

--- cs-popup.js
+++ cs-popup.js
@@ -3,9 +3,34 @@
  */
 
 function csClearCache(param) {
-  var cacheService = Components.classes["@mozilla.org/network/cache-service;1"]
-    .getService(Components.interfaces.nsICacheStorageService);
-  cacheService.clear();
+
+  function onGotSettings(settings) {
+    console.log(settings.options);
+    console.log(settings.dataToRemove);
+    console.log(settings.dataRemovalPermitted);
+  }
+
+  function onError(error) {
+    console.error(error);
+  }
+
+  browser.browsingData.settings().then(onGotSettings, onError);
+
+  // function notify() {
+  //   browser.notifications.create({
+  //     "type": "basic",
+  //     "title": "Cleared Cache",
+  //     "message": `Cleared all cache.`
+  //   });
+  // }
+
+  // browser.browsingData.removeCache({}).then(notify);
+
+
+  /* Old code */
+  // var cacheService = Components.classes["@mozilla.org/network/cache-service;1"]
+  //   .getService(Components.interfaces.nsICacheStorageService);
+  // cacheService.clear();
   // if ( param && param == 'ram' ) {
   //   cacheService.evictEntries(Components.interfaces.nsICache.STORE_IN_MEMORY);
   // } else if ( param && param == 'disk' ) {
